46011079a086dbc86144d4da0fc04b1da9875071
bakape <bakape@gmail.com>
2019-05-06 15:35:05 +0300

    Clean up pregenerated template

---
+++ README.md
@@ -0,0 +1,6 @@
+# brunhild
+experimental compressive Rust virtual DOM library
+
+Brunhild aims to provide a minimalistic fast virtual DOM implementation for use
+as is or for building higher level (for example, view-based) libraries and
+frameworks.
--- src/lib.rs
+++ src/lib.rs
@@ -7,13 +7,3 @@ use wasm_bindgen::prelude::*;
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
-#[wasm_bindgen]
-extern {
-    fn alert(s: &str);
-}
-
-#[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, brunhild!");
-}
